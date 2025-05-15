45ab47217a0d221ab76fd17e75869aed9c42428a
Ranc0 <raniali12813@gmail.com>
2025-05-15 13:57:38 +0300

    fixed bug in models

--- base/views/__init__.py
+++ base/views/__init__.py
@@ -3,10 +3,6 @@ from .pet import *
 from .account import * 
 from .vaccination import *
 from .post import *
-<<<<<<< Updated upstream
-from .product import *
-=======
 from .product import *
 from .homepage import *
 from .store import *
->>>>>>> Stashed changes
