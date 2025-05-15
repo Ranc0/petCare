e7db81d3af2e4560f237cfde3a5df23c51b6c3dc
Ranc0 <raniali12813@gmail.com>
2025-05-15 13:36:22 +0300

    Squashed commit of the following:
    commit 930b82108ea0654be4a291c6e9d6d4e76b992e77
    Merge: 784c2065 5f83efd3
    Author: Ranc0 <raniali12813@gmail.com>
    Date:   Thu May 15 04:19:23 2025 +0300
    
        Merge branch 'main' into Lina
    
    commit 5f83efd39899b9494e06b00c48c8a0a2d2626263
    Author: Ranc0 <raniali12813@gmail.com>
    Date:   Thu May 15 04:18:59 2025 +0300
    
        Squashed commit of the following:
    
        commit cdb95b095a26368e49bf0dad014aaed25ca87379
        Author: Ranc0 <raniali12813@gmail.com>
        Date:   Thu May 15 04:18:05 2025 +0300
    
            fixed merge problem
    
    commit 784c2065b5c36fd7801150079c45508d45e22071
    Merge: 885e8e04 e2f15205
    Author: Ranc0 <raniali12813@gmail.com>
    Date:   Thu May 15 04:10:58 2025 +0300
    
        Merge branch 'main' into Lina
    
    commit 885e8e046f7b21f0e05cf584e8ef6eabdc7ac70d
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Tue May 13 08:58:55 2025 +0300
    
        add product editing
    
    commit 474130b5898addae7b4605d0a7189f8e85f2cf2c
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Thu Mar 20 18:47:53 2025 +0300
    
        Homepage view added
    
    commit 1afdc6bb691868ed9c785e3d88cf3e2e0d90832b
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Thu Mar 20 17:06:25 2025 +0300
    
        Product ( getters & filters )
    
    commit 04386b85aa2e9c838a2d676233d036b3bc744d01
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Mon Mar 17 17:35:58 2025 +0300
    
        Add & delete product
    
    commit f3c9f532c25ced0afcd271ca966d22846c167e29
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Mon Mar 17 17:12:49 2025 +0300
    
        Edit birth_date field & serach
    
    commit 0d15448dcf35a2e03e019c945b3cc09fb52d6696
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Mon Mar 17 16:00:13 2025 +0300
    
        Edit adoption post ( for pet photo )
    
    commit eca258bbe06497d50e24966c44cce65d27f10211
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Mon Mar 17 14:53:46 2025 +0300
    
        Pet photo implementation
    
    commit eefd16bba46db503feb21ca985e2aeb56ba4fc9e
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sun Mar 16 16:21:14 2025 +0300
    
        Adoption post views little changes
    
    commit df7ebd24632c5ef83ee27acc2dd277ba2e5a89fc
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sun Mar 16 15:25:27 2025 +0300
    
        photo field added to post
    
    commit e4f45ffaa6099fb0a09f4d095f2eeb34418685b8
    Author: Ranc0 <raniali12813@gmail.com>
    Date:   Sat Mar 15 18:33:08 2025 +0300
    
        updated some little things
    
    commit 5053d5bd5b17c4d075de9b9ac8d404c0bb9a46cb
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sat Mar 15 17:42:14 2025 +0300
    
        Revert "Edit adoption post ( filtering )"
    
        This reverts commit 7efbda000375d98570c1de657658e1a43adeb55d.
    
    commit 1e6c0ff7b40b8033dac17f323e51f02a7855ce56
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sat Mar 15 17:39:39 2025 +0300
    
        Revert "Edit adoption post ( filtering )"
    
        This reverts commit 7efbda000375d98570c1de657658e1a43adeb55d.
    
    commit 7efbda000375d98570c1de657658e1a43adeb55d
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sat Mar 15 16:42:33 2025 +0300
    
        Edit adoption post ( filtering )
    
    commit 1252e9a9d85bb2598d035355efaf0e4e6fa56a3a
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sat Mar 15 15:53:38 2025 +0300
    
        Edit update pet view ( sending all pet data )
    
    commit b9ec6fc4d3bda2841ed2d445eba62c8b0c989fe0
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Sat Mar 15 15:13:20 2025 +0300
    
        Edit sign up user ( password registration )
    
    commit 2b4ea1f91357761268559da54c1f33a03f293540
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Fri Mar 14 14:53:52 2025 +0300
    
        Adoption post ( delete, get all, get post and serializer)
    
    commit 17344786832f27b30f0587ff96bea54253b68521
    Author: Lina-Alrashid <AlrashidLina77@gmail.com>
    Date:   Fri Mar 14 11:33:39 2025 +0300
    
        Account and pet views

---
+++ base/views/homepage.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.permissions import IsAuthenticated
+from ..models import AdoptionPost, BreedingPost , Product
+from django.contrib.auth.models import User
+from ..serializers import AdoptionPostSerializer, BreedingPostSerializer, ProductSerializer
+from rest_framework import status
+
+@api_view(['GET'])
+def homepage (request):
+    adoption_posts = AdoptionPost.objects.all()[:10]
+    adoption_posts = AdoptionPostSerializer(adoption_posts, many = True).data
+    breeding_posts = BreedingPost.objects.all()[:10]
+    breeding_posts = BreedingPostSerializer(breeding_posts, many = True).data
+    products = Product.objects.all().order_by('price')[:10]
+    products = ProductSerializer(products, many = True).data
+    response = {'adoption_posts':adoption_posts}
+    response.update({'breeding_posts':breeding_posts})
+    response.update({'products':products})
+    return Response (response, status= status.HTTP_200_OK)
--- base/views/post.py
+++ base/views/post.py
@@ -1,9 +1,10 @@
+import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view ,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from ..models import Pet  , AdoptionPost , BreedingPost
 from django.contrib.auth.models import User
-from ..serializers import PetSerializer
+from ..serializers import PetSerializer, AdoptionPostSerializer
 from rest_framework import status
 
 
@@ -19,11 +20,89 @@ def add_adoption_post (request , id):
     details = request.data["details"]
     AdoptionPost.objects.create(pet = pet , user = user , details = details)
     serialized_pet = PetSerializer( pet, many=False).data
-    serialized_pet.update({"details":details})
-    return Response(serialized_pet , status=status.HTTP_201_CREATED)
+    post = serialized_pet
+    post.update({"photo":pet.photo.url if pet.photo else None})
+    post.update({"details":details})
+    return Response(post , status=status.HTTP_201_CREATED)
 
 #delete post should be made 
 #no need to make a view for updating 
 #a getter should be made to list all the posts available
 #getters can be separated for dogs and cats ( filters ) 
 # same process for breeding posts (I made the model for you)
+
+@permission_classes([IsAuthenticated])
+@api_view(['DELETE'])
+def delete_adoption_post (request , id):
+    adoption_post = AdoptionPost.objects.filter(id = id)
+    if not adoption_post:
+        return Response({"message" : "no such id"} , status = status.HTTP_404_NOT_FOUND)
+    if adoption_post.user != request.user:
+        return Response({"message":"user does not have this post"}, status = status.HTTP_401_UNAUTHORIZED)
+    adoption_post.delete()
+    return Response({"message" : "post deleted successfully"})
+
+@permission_classes([IsAuthenticated])
+@api_view(['GET'])
+def get_adoption_posts (request):
+    posts = AdoptionPost.objects.all()
+    response = []
+    for post in posts:
+        pet = Pet.objects.get(id = post.pet_id)
+        serialized_pet = PetSerializer(pet).data
+        username = post.user.username
+        holder = serialized_pet
+        holder.update({"photo":pet.photo.url if pet.photo else None})
+        holder.update({"username":username})
+        holder.update({"details":post.details})
+        response.append(holder)
+    return Response(response, status= status.HTTP_200_OK)
+
+@permission_classes([IsAuthenticated])
+@api_view(['GET'])
+def get_adoption_post (request, id):
+    post = AdoptionPost.objects.filter(id = id)
+    if not post:
+        return Response({"message":"no such id"}, status= status.HTTP_404_NOT_FOUND)
+    post = post[0]
+    username = post.user.username
+    pet = Pet.objects.get(id = post.pet_id)
+    serialized_pet = PetSerializer(pet).data
+    response = serialized_pet
+    response.update({"photo":pet.photo.url if pet.photo else None})
+    response.update({"username":username})
+    response.update({"datails":post.details})
+    return Response(response, status= status.HTTP_200_OK)
+
+@permission_classes([IsAuthenticated])
+@api_view(['POST'])
+def adoption_filter (request):
+    filter_params = {
+        'type': request.data.get('type',None),
+        'breed': request.data.get('breed',None),
+        'gender': request.data.get('gender',None),
+    }
+    birth_date = request.data.get('birth_date',None)
+    if birth_date:
+        try:
+            birth_date_obj = datetime.datetime.strptime(birth_date, '%Y-%m-%d').date()
+            #print(birth_date_obj)
+            #print( Pet.objects.get(id=9).birth_date)
+            filter_params['birth_date__lte'] = birth_date_obj
+        except ValueError:
+            return Response({"message": "Invalid date format. Use YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+    filter_params = {key: value for key, value in filter_params.items() if value is not None}
+    pets = Pet.objects.filter(**filter_params)
+    response = []
+    for pet in pets:
+        post = AdoptionPost.objects.filter(pet_id = pet.id)
+        if post:
+            post = post[0]
+            username = pet.user.username
+            serialized_pet = PetSerializer(pet).data
+            holder = serialized_pet
+            holder.update({"photo":pet.photo.url if pet.photo else None})
+            holder.update({"username":username})
+            holder.update({"details":post.details})
+            response.append(holder)
+    return Response(response, status= 200)
